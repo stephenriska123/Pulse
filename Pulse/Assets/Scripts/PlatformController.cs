9c4cd0e90fa10a5bd843a3e7c223aab33736fbd3
stephenriska123 <stephenriska123@gmail.com>
2019-11-30 23:09:51 -0500

    music and spikes

---
+++ Pulse/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private AudioSource musicController;
+    private GameObject player;
+    private bool snapReady;
+    public static bool snapStarted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        endPosition = transform.position;
+        musicController = GameObject.FindWithTag("MusicController").GetComponent<AudioSource>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        endPosition.y = startPosition.y + 2 * Mathf.Sin(Time.time * 1.575f);
+        if (!musicController.isPlaying && endPosition.y > 3.99)
+        {
+            musicController.Play();
+        }
+        transform.position = endPosition;
+
+        if (player.transform.position.z > 5.5 && !snapReady)
+        {
+            Invoke("snaps", musicController.clip.length - musicController.time);
+            snapReady = true;
+        }
+    }
+
+    void snaps()
+    {
+        var snap = Resources.Load("2") as AudioClip;
+        musicController.clip = snap;
+        musicController.Play();
+        snapStarted = true;
+    }
+}
---
+++ Pulse/Assets/Scripts/SpikeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour
+{
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private bool snapStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        endPosition = new Vector3(transform.position.x,
+                                  transform.position.y < 3.5f ? transform.position.y + 1.5f : transform.position.y - 2.5f,
+                                  transform.position.z);
+        Debug.Log(endPosition.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlatformController.snapStarted && !snapStart)
+        {
+            InvokeRepeating("snapDown", 1, 2);
+            InvokeRepeating("snapUp", 2, 2);
+            snapStart = true;
+        }
+    }
+
+    void snapDown()
+    {
+        transform.position = endPosition;
+    }
+
+    void snapUp()
+    {
+        transform.position = startPosition;
+    }
+}
